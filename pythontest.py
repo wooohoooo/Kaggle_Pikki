404f4494cac399d3d48e22cb438af58e81c12c63
wooohoooo <wooohooo@gmx.de>
2015-05-09 20:11:29 +0200

    Create pythontest.py

---
+++ pythontest.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+import otto_Pikki as ot
+clf2 = RandomForestClassifier(n_estimators = 4000, n_jobs=-1, max_depth = 7)
+clf = GradientBoostingClassifier(n_estimators = 1000, subsample = .8, max_features = 20,max_depth = 10)
+n1 = ot.make_guess_csv(clf,name='turkeytest')
+n2 = ot.make_guess_csv(clf2,name='turkeytest')
+
+ot.combine_results(clf_list=[n1,n2])
